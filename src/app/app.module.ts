caabc2aba9c761793d20f3918639a07e82e317b4
Mark Wilhelm <mark.wilhelm@artisancreek.com>
2020-02-24 00:52:32 -0600

    add most angular material interaction

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -15,6 +15,10 @@ import { CurrentTrainingComponent } from './training/current-training/current-tr
 import { NewTrainingComponent } from './training/new-training/new-training.component';
 import { PastTrainingComponent } from './training/past-training/past-training.component';
 import { WelcomeComponent } from './welcome/welcome.component';
+import { HeaderComponent } from './navigation/header/header.component';
+import { SidenavListComponent } from './navigation/sidenav-list/sidenav-list.component';
+
+import { StopTrainingComponent } from './training/current-training/stop-training.component';
 
 @NgModule({
   declarations: [
@@ -25,7 +29,10 @@ import { WelcomeComponent } from './welcome/welcome.component';
     CurrentTrainingComponent,
     NewTrainingComponent,
     PastTrainingComponent,
-    WelcomeComponent
+    WelcomeComponent,
+    HeaderComponent,
+    SidenavListComponent,
+    StopTrainingComponent
   ],
   imports: [
     BrowserModule,
@@ -36,6 +43,7 @@ import { WelcomeComponent } from './welcome/welcome.component';
     FormsModule, ReactiveFormsModule
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
+  entryComponents: [StopTrainingComponent]
 })
 export class AppModule { }
--- src/app/material.module.ts
+++ src/app/material.module.ts
@@ -8,6 +8,12 @@ import { MatNativeDateModule } from '@angular/material/core';
 import { MatCheckboxModule } from '@angular/material/checkbox';
 import { MatSidenavModule } from '@angular/material/sidenav';
 import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatListModule } from '@angular/material/list';
+import { MatTabsModule } from '@angular/material/tabs';
+import { MatCardModule } from '@angular/material/card';
+import { MatSelectModule } from '@angular/material/select';
+import { MatProgressSpinnerModule } from '@angular/material/progress-spinner';
+import { MatDialogModule } from '@angular/material/dialog';
 
 @NgModule({
     imports: [
@@ -15,16 +21,18 @@ import { MatToolbarModule } from '@angular/material/toolbar';
         MatIconModule,
         MatFormFieldModule,
         MatInputModule, MatDatepickerModule, MatNativeDateModule,
-        MatCheckboxModule, 
-        MatSidenavModule, MatToolbarModule
+        MatCheckboxModule, MatDialogModule,
+        MatSidenavModule, MatToolbarModule, MatListModule, MatTabsModule,
+        MatCardModule, MatSelectModule, MatProgressSpinnerModule
     ],
     exports: [
         MatButtonModule,
         MatIconModule,
         MatFormFieldModule,
         MatInputModule, MatDatepickerModule, MatNativeDateModule,
-        MatCheckboxModule, 
-        MatSidenavModule, MatToolbarModule
+        MatCheckboxModule, MatDialogModule,
+        MatSidenavModule, MatToolbarModule, MatListModule, MatTabsModule,
+        MatCardModule, MatSelectModule, MatProgressSpinnerModule
     ]
 })
 export class MaterialModule {}
